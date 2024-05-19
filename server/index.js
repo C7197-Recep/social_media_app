9dbeee137ecc771be4f4d6f4d553f18b3dae2172
C7197Recep <yalcinr896@gmail.com>
2024-05-19 19:13:43 +0300

    feat (routes): add auth route

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ import morgan from "morgan";
 /*Dosya ve klasör sınırlama için*/
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
 import {register} from "./controllers/auth.js";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -41,6 +42,9 @@ const upload = multer({storage});
 
 /*ROUTES WITH FILES*/
 app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
 
 /* MONGOOSE SETUP*/
 const PORT= process.env.PORT || 6001;
